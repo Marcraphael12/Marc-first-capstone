06ec6ffe6caafccfde0bbc2538908aa95cf3f26b
Marc Raphael <marcraphael979@gmail.com>
2021-09-14 17:28:56 +0100

    Add mobile menu

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 
+const nav = document.querySelector('.nav-menu');
+const body = document.querySelector('body');
+
+const btn = document.querySelector('.menu');
+const cls = document.querySelector('.cross');
+
+btn.addEventListener('click', () => {
+  body.appendChild(nav);
+  nav.classList.add('show');
+  nav.classList.remove('.nav-menu');
+  btn.style.display = 'none';
+})
+
+cls.addEventListener('click', () =>{
+  nav.classList.remove('show');
+  nav.classList.add('.nav-menu');
+  btn.style.display = 'initial';
+})
